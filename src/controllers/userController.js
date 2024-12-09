772e1725dd868e09dc940108240914ad8411f108
Sounak Saha <sounaksaha@virtualemployee.com>
2024-12-09 13:20:59 +0530

    register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,14 @@
 const User = require("../models/user");
 const dotenv = require("dotenv");
 const twilio = require("twilio");
-require('dotenv').config();
+require("dotenv").config();
 
 dotenv.config();
 
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 const checkNumber = async (req, res) => {
   const { number } = req.params;
@@ -15,12 +18,12 @@ const checkNumber = async (req, res) => {
     if (user) {
       // If user exists, send OTP for Login
       const verification = await client.verify.v2
-      .services(process.env.TWILIO_VERIFY_SERVICE_SID)
-      .verifications.create({ to: number, channel: "sms" });
+        .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+        .verifications.create({ to: number, channel: "sms" });
 
       return res
         .status(200)
-        .json({ message: "Number is registered, OTP sent", registered: true });
+        .json({ message: "Number is registered, OTP sent", registered: true,user });
     } else {
       // If user does not exist, proceed to registration
       return res
@@ -32,36 +35,46 @@ const checkNumber = async (req, res) => {
   }
 };
 
-
 const registerUser = async (req, res) => {
-    const {number}=req.params;
-    const {  firstName, lastName, dob, postalCode, email, gender } = req.body;
-  
-    try {
-      // Check if number already exists
-      const existingUser = await User.findOne({ number });
-      if (existingUser) {
-        return res.status(400).json({ message: 'Number is already registered' });
-      }
-  
-      // Send OTP via Twilio
-    
-      const verification = await client.verify.v2
-        .services(process.env.TWILIO_VERIFY_SERVICE_SID)
-        .verifications.create({ to: number, channel: "sms" });
+  const { number } = req.params;
+  const { firstName, lastName, dob, postalCode, email, gender } = req.body;
 
-
-      // Temporarily save user details in the database (without verification)
-      const newUser = new User({ number, firstName, lastName, dob, postalCode, email, gender });
-      await newUser.save();
-  
-      res.status(200).json({ message: 'OTP sent successfully, complete verification', userId: newUser._id });
-    } catch (error) {
-      res.status(500).json({ message: 'Error sending OTP', error });
+  try {
+    // Check if number already exists
+    const existingUser = await User.findOne({ number });
+    if (existingUser) {
+      return res.status(409).json({ message: "Number is already registered" });
     }
-  };
+
+    // Send OTP via Twilio
+    const verification = await client.verify.v2
+      .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+      .verifications.create({ to: number, channel: "sms" });
+
+    // Temporarily save user details in the database (without verification)
+    const newUser = new User({
+      number,
+      firstName,
+      lastName,
+      dob,
+      postalCode,
+      email,
+      gender,
+    });
+    await newUser.save();
+
+    res
+      .status(200)
+      .json({
+        message: "OTP sent successfully, complete verification",
+        userId: newUser._id,
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Error sending OTP", error });
+  }
+};
 
 module.exports = {
   checkNumber,
-  registerUser
+  registerUser,
 };
